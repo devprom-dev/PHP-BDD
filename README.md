d26f8d8c534bc204b6a12888c67c1cf0376b67fb
Richard <hoffmann_richard@inbox.ru>
2016-09-13 18:14:03 +0600

    #3 Собрал докер контейнер.

--- README.md
+++
@@ -1,4 +0,0 @@
-php-bdd-src
-===========
-
-A Symfony project created on August 24, 2016, 10:40 am.
--- src/PHPBDD/PosterBundle/PHPBDDPosterBundle.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace PHPBDD\PosterBundle;
-
-use Symfony\Component\HttpKernel\Bundle\Bundle;
-
-class PHPBDDPosterBundle extends Bundle
-{
-}
