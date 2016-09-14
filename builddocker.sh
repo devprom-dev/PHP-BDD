a0d57618fe5bc5ca0252cb8fb749068ae64c3b2f
Richard <hoffmann_richard@inbox.ru>
2016-09-14 17:12:14 +0600

    #4 Дал кастомное имя контейнеру с проектом.

--- builddocker.sh
+++ builddocker.sh
@@ -4,7 +4,7 @@ cd base/php-bdd/
 composer install
 cd ../../
 docker-compose up -d --build
-docker exec `docker ps -q -f name=repo_base_1` php app/console doctrine:database:drop --force --if-exists
-docker exec `docker ps -q -f name=repo_base_1` php app/console doctrine:database:create
-docker exec `docker ps -q -f name=repo_base_1` php app/console doctrine:schema:update --force
-docker exec `docker ps -q -f name=repo_base_1` php app/console doctrine:fixtures:load
+docker exec `docker ps -q -f name=php_bdd_base` php app/console doctrine:database:drop --force --if-exists
+docker exec `docker ps -q -f name=php_bdd_base` php app/console doctrine:database:create
+docker exec `docker ps -q -f name=php_bdd_base` php app/console doctrine:schema:update --force
+docker exec `docker ps -q -f name=php_bdd_base` php app/console doctrine:fixtures:load
