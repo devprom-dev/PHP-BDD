2c7aab6ce7657088eec267f18dd3bdb8aadf6dd4
Richard <hoffmann_richard@inbox.ru>
2016-09-14 12:01:17 +0600

    #3 Изменил скрипты создания докеров, теперь приложение сначала устанавливает все необходимые зависимости, а потом добавляется в докер.

--- builddocker.sh
+++ builddocker.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+cp parameters.yml base/php-bdd/app/config/
+cd base/php-bdd/
+composer install
+cd ../../
 docker-compose up -d --build
 docker exec `docker ps -q -f name=repo_base_1` php app/console doctrine:database:drop --force --if-exists
 docker exec `docker ps -q -f name=repo_base_1` php app/console doctrine:database:create
