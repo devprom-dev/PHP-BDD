3936f1093aef70912680fdf770337901804c2d69
Nikita Koval <me@nikitakoval.com>
2016-09-21 11:50:39 +0600

    #4 Поправил скрипты, сделал из соответствующими Jenkinsfile

--- cleandocker.sh
+++ cleandocker.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+# To delete not used docker images
 docker ps --filter status=dead --filter status=exited -aq | xargs -r docker rm -v
 docker images --no-trunc | grep '<none>' | awk '{ print $3 }' | xargs -r docker rmi
 docker volume ls -qf dangling=true | xargs -r docker volume rm
